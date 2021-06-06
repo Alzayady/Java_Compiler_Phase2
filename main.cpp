50520635bc1e1ff32ae344b1ba353c1487245520
unknown <mahmoud.manfy159@gmail.com>
2021-06-06 20:00:59 +0200

    parsing the input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "userInterface/File.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    File file("../input.txt");
+    try{
+        file.go();
+    } catch (std::string line) {
+        std::cout << "Error in input file please review it and try again." << std::endl;
+        std::cout << "In line " + line << std::endl;
+    }
+
     return 0;
 }
